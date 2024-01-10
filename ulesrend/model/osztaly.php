7617528a81efbd676f90c44221d6a5a7317e5a73
HMark6 <144675870+HMark6@users.noreply.github.com>
2024-01-10 18:07:36 +0100

    Update osztaly.php

--- ulesrend/model/osztaly.php
+++ ulesrend/model/osztaly.php
@@ -1,24 +1,41 @@
 <?php
 
-class Osztaly{
+class Osztaly {
 
-    function getOsztaly(){
+    function getOsztaly() {
+    
         $sql = "SELECT id, nev, sor, oszlop FROM osztaly ORDER BY sor, oszlop;";
         $result = DataBase::$conn->query($sql);
-
+        
         return $result;
     }
 
-    function getUser($id){
-        $sql = "SELECT id, nev, sor, oszlop FROM osztaly WHERE id=".$id;
-        $result = DataBase::$conn->query($sql);
 
+
+    function getUser($id) {
+        /* Prepared statement, stage 1: prepare */
+        $stmt = DataBase::$conn->prepare("SELECT nev, sor, oszlop FROM osztaly WHERE id = ?");
+
+        /* Prepared statement, stage 2: bind and execute */
+        $stmt->bind_param("i", $id); // "i" means that $id is bound as an integer
+
+        $stmt->execute();
+        $result = $stmt->get_result();
+        
         return $result;
     }
+    
+    function updateOsztaly() {
+        $sql = "UPDATE osztaly SET nev = ? WHERE id = ".$_SESSION['id'];
+        /* Prepared statement, stage 1: prepare */
+        $stmt = DataBase::$conn->prepare($sql);
+        
+        /* Prepared statement, stage 2: bind and execute */
+        $stmt->bind_param("s", $_POST['modositandoNev']); // 
 
-    function updateOsztaly(){
-        $sql = "UPDATE osztaly SET nev = '".$_POST['modositandoNev']."' WHERE id = ".$_SESSION['id'];
-        if($result = DataBase::$conn->query($sql)) {
+        $stmt->execute();
+
+        if($result = $stmt->affected_rows) {
             $msg = "A név módosításra került";
         }
         else {
@@ -30,29 +47,38 @@ class Osztaly{
         }
         return $msg;
     }
-    /**
-     * ellenőrizni a felhasználót, beléptetni, sessiont ír, vagy hibát ad vissza
-     */
 
-    function checkLogin($msg){
-        $sql = "SELECT jelszo, id, nev FROM osztaly WHERE felhasznalonev = '".$_POST['felhasznalonev']."';";
-        $result = DataBase::$conn->query($sql);
+    /**
+     * ellenőrzi a felhasználót, belépteti, sessiont ír, vagy hibát ad vissza
+     */
+    function checkLogin($msg) {
+        $sql = "SELECT jelszo, id, nev FROM osztaly WHERE felhasznalonev = ?";
+        
+        $stmt = DataBase::$conn->prepare($sql);
+        
+        $stmt->bind_param("s", $_POST['felhasznalonev']);
+
+        $stmt->execute();
+
+        $result = $stmt->get_result();
 
         if ($result->num_rows > 0) {
-            if($row = $result->fetch_assoc()){
-                    if($row['jelszo'] == md5($_POST['jelszo'])){
-                        $_SESSION['felhasznalonev'] = $_POST['felhasznalonev'];
-                        $_SESSION['nev'] = $row['nev'];
-                        $_SESSION['id'] = $row['id'];
-                    }
-                    else{
-                        $msg .="A felhasználóhoz megadott jelszó nem érvényes. ";
-                    }
+            if($row = $result->fetch_assoc()) {
+                if($row['jelszo'] == md5($_POST['jelszo'])) {
+                    $_SESSION['felhasznalonev'] = $_POST['felhasznalonev'];
+                    $_SESSION['nev'] = $row['nev'];
+                    $_SESSION['id'] = $row['id'];
+                }
+                else {
+                    $msg .= "A felhasználóhoz megadott jelszó nem érvényes. ";
+                }
             }
-        }else{
-            $msg .="A megadott ".$_POST['felhasznalonev']." felhasználónév nem található. ";
+        }
+        else {
+            $msg .= "A megadott ".$_POST['felhasznalonev']." felhasználónév nem található. ";
         }
         return $msg;
     }
 }
+
 ?>
